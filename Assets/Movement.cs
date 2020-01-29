55bf2e56abcba6f0d2754ede50dde999dc7cd1da
lstledg <linnea.stledger@elev.ga.lbs.se>
2020-01-29 22:30:46 +0100

    Game

---
+++ Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        gameObject.transform.Translate(new Vector3(0, 0, speed));
+    }
+
+}
